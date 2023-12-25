d522ff41a52c12ab1f0f37e578105be7ecbbb9fb
Asia <dobrowolska.krakow@gmail.com>
2023-12-25 14:52:33 +0100

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/multi-step-form">
+    <BrowserRouter basename="/Audiophile-ecommerce-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
