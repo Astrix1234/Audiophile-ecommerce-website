92576575bdb6ef182a282278ec401e807c74523e
Asia <dobrowolska.krakow@gmail.com>
2023-12-29 14:06:39 +0100

    made SectionMain for Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SectionNavCategories } from 'components/SectionNavCategories/SectionNavCategories';
 import { SectionProductsHome } from 'components/SectionProductsHome/SectionProductsHome';
+import { SectionMainHome } from 'components/SectionMainHome/SectionMainHome';
 
 export default function Home() {
   return (
     <>
+      <SectionMainHome />
       <SectionNavCategories />
       <SectionProductsHome />
     </>
