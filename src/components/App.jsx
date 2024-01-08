7790c7e00ff77863c0d90957096de844fe37d56d
Asia <dobrowolska.krakow@gmail.com>
2024-01-08 14:26:15 +0100

    customized app to download data from the server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { createContext, lazy, useState, useEffect } from 'react';
 import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-import data from '../data.json';
+import { getAllProducts } from 'api';
+import { Loader } from './Loader/Loader';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Category = lazy(() => import('../pages/category/Category'));
@@ -12,7 +13,25 @@ export const DataContext = createContext();
 
 export const App = () => {
   const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const location = useLocation();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await getAllProducts();
+        setData(response.data.products);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  console.log(data);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -31,6 +50,7 @@ export const App = () => {
   };
   return (
     <DataContext.Provider value={{ data, products, refreshProducts }}>
+      {isLoading && <Loader />}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Navigate replace to="/home" />} />
